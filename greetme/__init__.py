10955462a72fb9ae677ee5de9eb30be4ac427d36
Andreas Copan <avcopan@gmail.com>
2024-06-26 11:13:21 -0400

    Configures formatter and linter with extra checks
    Especially includes docstring checks

--- greetme/__init__.py
+++ greetme/__init__.py
@@ -1,6 +1,18 @@
+"""A library for welcoming others."""
+
+
 def string(name: str = "you") -> str:
+    """Build the welcome string.
+
+    :param name: The name of the one being welcomed, defaults to "you"
+    :return: The welcome string
+    """
     return f"Hey, {name.strip()}!"
 
 
-def do_it() -> None:
-    print(string())
+def do_it(name: str = "you") -> None:
+    """Welcome someone.
+
+    :param name: The name of the one being welcomed, defaults to "you"
+    """
+    print(string(name=name))
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for the greetme library."""
